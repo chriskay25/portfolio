50671704622c4da6d1c15e782eae14f0ff711800
Chris Kay <ckay84@protonmail.com>
2021-07-22 16:15:23 -0400

    Import Social Media comp, wrap right column in div for formatting

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,24 +1,28 @@
 import Avatar from './Avatar'
 import Column from './Column'
+import SocialMedia from './SocialMedia'
 
 const Intro = () => {
     return (
         <div className='intro'>
             <Column>
                 <h1 className='name'>Chris Kay</h1>
-                <div style={{height: '5px', width: '20%', background: 'var(--red'}} />
+                <div style={{height: '5px', width: '20%', background: 'var(--red)', marginBottom: '5px'}} />
+                <SocialMedia />
                 <Avatar />
             </Column>
             <Column>
-                <h2 style={{fontFamily: 'Montserrat'}}>Full Stack Developer</h2>
-                <p>
-                    Hello! My name is Chris Kay and I'm a full stack software engineer with a passion for technology.
-                    I'm interested in finding a position as a junior developer at a company where I can learn and grow
-                    as an engineer. 
-                </p>
+                <div className='intro-description'>
+                    <h2 style={{fontFamily: 'Montserrat'}}>Full Stack Developer</h2>
+                    <p style={{fontFamily: 'Manjari'}}>
+                        Hello! My name is Chris Kay and I'm a full stack software engineer with a passion for technology.
+                        I'm interested in finding a position as a junior developer at a company where I can learn and grow
+                        as an engineer. 
+                    </p>
+                    <button>My Resume</button>
+                </div>
             </Column>
         </div>
     )
 }
-
 export default Intro

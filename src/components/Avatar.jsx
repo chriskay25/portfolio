ad2638ff078b878e655af2014d280299c914196b
Chris Kay <ckay84@protonmail.com>
2021-07-22 14:07:51 -0400

    Add an Avatar component to hold img

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,11 @@
+import dev from '../dev-png-5.png'
+
+const Avatar = () => {
+    return (
+        <div className='avatar'>
+            <img src={dev} alt='dev-png' />
+        </div>
+    )
+}
+
+export default Avatar

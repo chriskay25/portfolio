67dff1816196a80dba852669179a5ced117e23a1
Chris Kay <ckay84@protonmail.com>
2021-07-22 14:10:12 -0400

    Add some content to Intro columns

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,22 @@
+import Avatar from './Avatar'
+import Column from './Column'
+
 const Intro = () => {
     return (
         <div className='intro'>
-            Intro
+            <Column>
+                <h1 className='name'>Chris Kay</h1>
+                <div style={{height: '5px', width: '20%', background: 'var(--red'}} />
+                <Avatar />
+            </Column>
+            <Column>
+                <h2 style={{fontFamily: 'Montserrat'}}>Full Stack Developer</h2>
+                <p>
+                    Hello! My name is Chris Kay and I'm a full stack software engineer with a passion for technology.
+                    I'm interested in finding a position as a junior developer at a company where I can learn and grow
+                    as an engineer. 
+                </p>
+            </Column>
         </div>
     )
 }

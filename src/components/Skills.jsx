94c6d26c58735bc96b94dec709db3a1b6e62247a
Chris Kay <ckay84@protonmail.com>
2021-07-24 18:41:43 -0400

    Add organization to the software stack list

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,16 +1,44 @@
 import Column from './Column'
+import rubyLogo from '../assets/ruby.png'
+import railsLogo from '../assets/rails-logo-2.png'
+import jsLogo from '../assets/js-logo.png'
+import cssLogo from '../assets/css-logo-1.png'
+import htmlLogo from '../assets/html-logo.png'
+import nodeLogo from '../assets/node-logo-3.png'
 
 const Skills = () => {
     return (
         <div className='skills'>
             <Column>
-                <h2>Technology I've Used</h2>
-                <ul style={{listStyle: 'none'}}>
-                    <li>Ruby / Rails</li>
-                    <li>Javascript</li>
-                    <li>CSS</li>
-                    <li>HTML</li>
-                </ul>
+                <div className='skills-tech'>
+                    <h2>My Software Stack</h2>
+                    <ul>
+                        <li>
+                            <div>Ruby</div>
+                            <img src={rubyLogo} alt='ruby logo' />
+                        </li>
+                        <li>
+                            <div>Rails</div>
+                            <img src={railsLogo} alt='rails logo' />
+                        </li>
+                        <li>
+                            <div>Javascript</div>
+                            <img src={jsLogo} alt='javascript logo' />
+                        </li>
+                        <li>
+                            <div>CSS</div>
+                            <img src={cssLogo} alt='css logo' />
+                        </li>
+                        <li>
+                            <div>HTML</div>
+                            <img src={htmlLogo} alt='html logo' />
+                        </li>
+                        <li>
+                            <div>Node.js</div>
+                            <img src={nodeLogo} alt='node logo' />
+                        </li>
+                    </ul>
+                </div>
             </Column>
             <Column>
                 <div className='skills-experience-container'>
@@ -22,7 +50,7 @@ const Skills = () => {
                 <div className='skills-experience-container'>
                     <span className='number-large'>5 </span>
                     <div>
-                        <span>Completed Projects</span> 
+                        <span>Portfolio Projects</span> 
                     </div>
                 </div>
             </Column>

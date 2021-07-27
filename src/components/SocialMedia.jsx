3e976dfc3836b5e0eaf83c072b9cccd114d7753e
Chris Kay <ckay84@protonmail.com>
2021-07-27 16:56:50 -0400

    Link social media imgs to external sites

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -2,10 +2,19 @@ import liLogo from '../assets/linkedin-32.png'
 import ghLogo from '../assets/gh-light-sm.png'
 
 const SocialMedia = () => {
+
+    const handleLiClick = () => {
+        window.location.href = 'https://linkedin.com/in/chris-kay-4060bb61'
+    }
+
+    const handleGhClick = () => {
+        window.location.href = 'https://github.com/chriskay25'
+    }
+
     return (
         <div className='social-media'>
-            <img src={liLogo} alt='linked-in-logo' />
-            <img src={ghLogo} alt='github-logo' />
+            <img src={liLogo} alt='linked-in-logo' onClick={() => handleLiClick} />
+            <img src={ghLogo} alt='github-logo' onClick={() => handleGhClick()} />
         </div>
     )
 }

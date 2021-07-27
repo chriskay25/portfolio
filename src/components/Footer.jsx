17ba7ea94279bacaf669ae5b3583be5304508894
Chris Kay <ckay84@protonmail.com>
2021-07-27 19:59:55 -0400

    Add email icon and an svg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,26 @@
 import SocialMedia from './SocialMedia'
+import { HiOutlineMail } from 'react-icons/hi'
 
 const Footer = () => {
     return (
         <div className='footer'>
             <div className='contact-me'>
                 <h3>Have a job or project? Talk to me.</h3>
-                <a href='mailto:ckay84@protonmail.com'>Send me an email   --></a>
+                <a href='mailto:ckay84@protonmail.com'>
+                    ckay84@protonmail.com
+                    <HiOutlineMail style={{marginLeft: '10px'}}/>
+                </a>
+            </div>
+
+            <div style={{height: '70px', display: 'flex', justifyContent: 'center'}}>
+                <svg height='60px' width='60px'>
+                    <polygon points='30 5, 5 20, 5 40, 30 55, 55 40, 55 20'
+                        stroke='var(--red)'
+                        strokeWidth='3px' />
+                    <text x='50%' y='50%' style={{transform: 'translate(-14px, 6px'}}>
+                        CK
+                    </text>
+                </svg>
             </div>
             <div className='thanks'>
                 <p>Thanks for visiting!</p>

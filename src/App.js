7d03183f4d08549e5725a86bcb6e0886008dec69
Chris Kay <ckay84@protonmail.com>
2021-07-27 14:37:52 -0400

    Add Footer to App comp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Intro from './components/Intro'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Intro />
       <Skills />
       <Projects />
+      <Footer />
     </div>
   );
 }

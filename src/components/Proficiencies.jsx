8ba3f7199a8280d43ed46a03e0bcd80a209aa5b3
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-08 14:53:00 -0400

    Create a proficiencies component

---
+++ src/components/Proficiencies.jsx
@@ -0,0 +1,21 @@
+const Proficiencies = () => {
+    return (
+        <div className='proficiencies'>
+            <h3>Relevant Skills & Knowledge</h3>
+            <ul>
+                <li>MVC Frameworks</li>
+                <li>Object Oriented Programming</li>
+                <li>SQL Databases (Sqlite and Postgress)</li>
+                <li>Authentication (Rails, JWT's, OAuth)</li>
+                <li>Single Page Applications</li>
+                <li>RESTful Applications</li>
+                <li>APIs</li>
+                <li>JSON</li>
+                <li>Git</li>
+                <li>Thunk</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Proficiencies

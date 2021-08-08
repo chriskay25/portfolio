a22f3dbd5f5916025bdc456bc8d82f14dfc23336
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-08 14:54:29 -0400

    Replace code in skills comp with new components, adjust css

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,71 +1,15 @@
 import Column from './Column'
-import rubyLogo from '../assets/ruby.png'
-import railsLogo from '../assets/rails-logo-3.png'
-import jsLogo from '../assets/js-logo.png'
-import cssLogo from '../assets/css-logo-1.png'
-import htmlLogo from '../assets/html-logo.png'
-import nodeLogo from '../assets/node-logo-3.png'
-import reactLogo from '../assets/react-logo.png'
-import reduxLogo from '../assets/redux-logo.png'
+import Stack from './Stack'
+import Proficiencies from './Proficiencies'
 
 const Skills = () => {
     return (
         <div className='skills'>
             <Column>
-                <div className='skills-tech'>
-                    <h2>My Software Stack</h2>
-                    <ul>
-                        <li>
-                            <div>Ruby</div>
-                            <img src={rubyLogo} alt='ruby logo' />
-                        </li>
-                        <li>
-                            <div>Rails</div>
-                            <img src={railsLogo} alt='rails logo' />
-                        </li>
-                        <li>
-                            <div>Javascript</div>
-                            <img src={jsLogo} alt='javascript logo' />
-                        </li>
-                        <li>
-                            <div>React</div>
-                            <img src={reactLogo} alt='react logo' />
-                        </li>
-                        <li>
-                            <div>Redux</div>
-                            <img src={reduxLogo} alt='redux-logo' />
-                        </li>
-                        <li>
-                            <div>Node.js</div>
-                            <img src={nodeLogo} alt='node logo' />
-                        </li>
-                        <li>
-                            <div>CSS</div>
-                            <img src={cssLogo} alt='css logo' />
-                        </li>
-                        <li>
-                            <div>HTML</div>
-                            <img src={htmlLogo} alt='html logo' />
-                        </li>
-                    </ul>
-                </div>
+                <Stack />
             </Column>
             <Column>
-                <div className='proficiencies'>
-                    <h3>Relevant Skills & Knowledge</h3>
-                    <ul>
-                        <li>MVC Frameworks</li>
-                        <li>Object Oriented Programming</li>
-                        <li>SQL Databases (Sqlite and Postgress)</li>
-                        <li>Authentication (Rails, JWT's, OAuth)</li>
-                        <li>Single Page Applications</li>
-                        <li>RESTful Applications</li>
-                        <li>APIs</li>
-                        <li>JSON</li>
-                        <li>Git</li>
-                        <li>Thunk</li>
-                    </ul>
-                </div>
+                <Proficiencies />
                 <div className='skills-experience-container'>
                     <span className='number-large'>2 </span>
                     <div>

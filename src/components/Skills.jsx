a887ac121492cc2758a91b2942e19b2ac7890de0
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-08 14:23:46 -0400

    Add to skills list

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -58,6 +58,8 @@ const Skills = () => {
                         <li>Object Oriented Programming</li>
                         <li>SQL Databases (Sqlite and Postgress)</li>
                         <li>Authentication (Rails, JWT's, OAuth)</li>
+                        <li>Single Page Applications</li>
+                        <li>RESTful Applications</li>
                         <li>APIs</li>
                         <li>JSON</li>
                         <li>Git</li>

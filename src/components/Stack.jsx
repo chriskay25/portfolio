3e50b777b9829cc150ba67df27e4195b943e0ddf
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-09 16:58:30 -0400

    Use map method and imported data to simplify comp

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,49 +1,16 @@
-import rubyLogo from '../assets/ruby.png'
-import railsLogo from '../assets/rails-logo-3.png'
-import jsLogo from '../assets/js-logo.png'
-import cssLogo from '../assets/css-logo-1.png'
-import htmlLogo from '../assets/html-logo.png'
-import nodeLogo from '../assets/node-logo-3.png'
-import reactLogo from '../assets/react-logo.png'
-import reduxLogo from '../assets/redux-logo.png'
+import data from '../assets/data.js'
 
 const Stack = () => {
     return (
         <div className='stack'>
             <h2>My Software Stack</h2>
             <ul>
-                <li>
-                    <div>Ruby</div>
-                    <img src={rubyLogo} alt='ruby logo' />
-                </li>
-                <li>
-                    <div>Rails</div>
-                    <img src={railsLogo} alt='rails logo' />
-                </li>
-                <li>
-                    <div>Javascript</div>
-                    <img src={jsLogo} alt='javascript logo' />
-                </li>
-                <li>
-                    <div>React</div>
-                    <img src={reactLogo} alt='react logo' />
-                </li>
-                <li>
-                    <div>Redux</div>
-                    <img src={reduxLogo} alt='redux-logo' />
-                </li>
-                <li>
-                    <div>Node.js</div>
-                    <img src={nodeLogo} alt='node logo' />
-                </li>
-                <li>
-                    <div>CSS</div>
-                    <img src={cssLogo} alt='css logo' />
-                </li>
-                <li>
-                    <div>HTML</div>
-                    <img src={htmlLogo} alt='html logo' />
-                </li>
+                {data.map((item, index) => (
+                    <li>
+                        <div>{item.name}</div>
+                        <img src={item.img} alt={item.alt} />
+                    </li>
+                ))}
             </ul>
         </div>
     )

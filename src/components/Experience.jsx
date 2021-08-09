71344ac62187c6ddf06caab2ed4fa0832d14e590
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-09 15:52:07 -0400

    Create an experience component

---
+++ src/components/Experience.jsx
@@ -0,0 +1,20 @@
+const Experience = () => {
+    return (
+        <div className='experience'>
+            <div className='experience-container'>
+                <div className='experience-content'>
+                    <span className='number-large'>2</span>
+                    <span>Years of Experience</span>
+                </div>
+            </div>
+            <div className='experience-container'>
+                <div className='experience-content'>
+                    <span className='number-large'>5</span>
+                    <span>Portfolio Projects</span> 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Experience

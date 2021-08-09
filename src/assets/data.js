9358b9e41cda7faef86ed06ec28940de1a0b7f9b
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-09 16:57:39 -0400

    Create a data file to help simplify skills component

---
+++ src/assets/data.js
@@ -0,0 +1,53 @@
+import rubyLogo from './ruby.png'
+import railsLogo from './rails-logo-3.png'
+import jsLogo from './js-logo.png'
+import cssLogo from './css-logo-1.png'
+import htmlLogo from './html-logo.png'
+import nodeLogo from './node-logo-3.png'
+import reactLogo from './react-logo.png'
+import reduxLogo from './redux-logo.png'
+
+const stackData = [
+    {
+        name: 'Ruby',
+        img: rubyLogo,
+        alt: 'ruby logo'
+    },
+    {
+        name: 'Rails',
+        img: railsLogo,
+        alt: 'rails logo'
+    },
+    {
+        name: 'Javascript',
+        img: jsLogo,
+        alt: 'javascript logo'
+    },
+    {
+        name: 'React',
+        img: reactLogo,
+        alt: 'react logo'
+    },
+    {
+        name: 'Redux',
+        img: reduxLogo,
+        alt: 'redux logo'
+    },
+    {
+        name: 'Node.js',
+        img: nodeLogo,
+        alt: 'node logo'
+    },
+    {
+        name: 'CSS',
+        img: cssLogo,
+        alt: 'css logo'
+    },
+    {
+        name: 'HTML',
+        img: htmlLogo,
+        alt: 'html logo'
+    }
+]
+
+export default stackData

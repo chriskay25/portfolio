3d0405940161e2884d7df2c56a6339bf12c328c7
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-10 14:53:17 -0400

    Extract data from Stack into separate file

--- src/assets/data.js
+++ src/assets/data.js
@@ -9,44 +9,60 @@ import reduxLogo from './redux-logo.png'
 
 const stackData = [
     {
+        id: 1,
         name: 'Ruby',
         img: rubyLogo,
-        alt: 'ruby logo'
+        alt: 'ruby logo',
+        description: "Ruby is a high level general purpose programming language with a focus on object orientation. Ruby was written with the goal to make programmers happy by providing flexibility. This is the first language I learned, and I still have a particular fondness for it."
     },
     {
+        id: 2,
         name: 'Rails',
         img: railsLogo,
-        alt: 'rails logo'
+        alt: 'rails logo',
+        description: "Rails is a framework for developing web applications. Valuing convention over configuration makes the framework easier to understand and navigate for newcomers and experienced developers. Ruby and Rails have a dedicated following and a very active community, which is the main reason they're still popular today."
     },
     {
-        name: 'Javascript',
+        id: 3,
+        name: 'JavaScript',
         img: jsLogo,
-        alt: 'javascript logo'
+        alt: 'javascript logo',
+        description: "JavaScript is a prototype-based dynamic language, supporting object-oriented, imperative, and declarative styles. The uses for JS have grown over the years, but it is most well known as the scripting language for the web."
     },
     {
+        id: 4,
         name: 'React',
         img: reactLogo,
-        alt: 'react logo'
+        alt: 'react logo',
+        description: "React is a free and open-source front-end JavaScript library developed by Facebook. It allows developers to build user interfaces with reusable components written in JSX (a mixture of JavaScript and HTML)."
     },
     {
+        id: 5,
         name: 'Redux',
         img: reduxLogo,
-        alt: 'redux logo'
+        alt: 'redux logo',
+        description: "Redux is an open-source JavaScript library for managing application state. It is most commonly used with libraries such as React for building user interfaces. The main benefit of using Redux is centralizing an application's state and logic."
     },
     {
+        id: 6,
         name: 'Node.js',
         img: nodeLogo,
-        alt: 'node logo'
+        alt: 'node logo',
+        description: "Node.js is a free, open source server environment. It runs single-threaded, non-blocking, asynchronus programming, making it very memory efficient. Node.js allows developers to run JavaScript on the server. Due to its ease of use, npm has helped the Node.js ecosystem to proliferate."
     },
     {
+        id: 7,
         name: 'CSS',
         img: cssLogo,
-        alt: 'css logo'
+        alt: 'css logo',
+        description: "CSS (Cascading Style Sheets) is a stylesheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the web, alongside JavaScript and HTML."
     },
     {
+        id: 8,
         name: 'HTML',
         img: htmlLogo,
-        alt: 'html logo'
+        alt: 'html logo',
+        description: "HTML (HyperText Markup Language) is the standard markup language for documents designed to be displayed in a web browser. It is the most basic building block of the web, defining the meaning and structure of web content."
     }
 ]
 

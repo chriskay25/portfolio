7cfba7af6b66082dd8ee08ddf1c1fa4578275ea6
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-10 14:54:36 -0400

    Iterate over data to simplify comp, use motion for ul and li's to make dynamic

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,17 +1,31 @@
+import React, { useState } from 'react'
 import data from '../assets/data.js'
+import Card from './Card'
+import { motion, AnimateSharedLayout } from 'framer-motion'
 
 const Stack = () => {
+    const [selected, setSelected] = useState(false)
+
+    const handleSelected = (id) => {
+        if (selected) {
+            setSelected(false)
+            return
+        }
+        setSelected(id)
+    }
+
     return (
         <div className='stack'>
             <h2>My Software Stack</h2>
-            <ul>
-                {data.map((item, index) => (
-                    <li>
-                        <div>{item.name}</div>
-                        <img src={item.img} alt={item.alt} />
-                    </li>
-                ))}
-            </ul>
+            <AnimateSharedLayout type='crossfade'>
+                <motion.ul animate={selected ? {blur: '4px'} : {blur: 0}} layout>
+                    {data.map((item) => (
+                        <li key={item.id} onClick={() => handleSelected(item.id)}>
+                            <Card key={item.id} item={item} selected={selected === item.id} />
+                        </li>
+                    ))}
+                </motion.ul>
+            </AnimateSharedLayout>
         </div>
     )
 }

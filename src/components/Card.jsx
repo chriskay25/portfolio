b27f0081a63a8dfb6471dee8795366bf61873caf
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-12 23:55:48 -0400

    Use AnimatePresence for smooth transition between elements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const Card = ({ item, selected }) => {
     return (
@@ -8,13 +8,15 @@ const Card = ({ item, selected }) => {
                 <motion.div layoutId={item.name}>{item.name}</motion.div>
                 <motion.img layoutId={item.img} src={item.img} alt={item.alt} />
             </motion.div>
-            {selected && (
-                <motion.div className='selected-card' layoutId={item.id}>
-                    <motion.img src={item.img} alt={item.alt} layoutId={item.img} />
-                    <motion.span layoutId={item.name}>{item.name}</motion.span>
-                    <div>{item.description}</div>
-                </motion.div>
-            )}
+            <AnimatePresence>
+                {selected && (
+                    <motion.div className='selected-card' layoutId={item.id}>
+                        <motion.img src={item.img} alt={item.alt} layoutId={item.img} />
+                        <motion.span layoutId={item.name}>{item.name}</motion.span>
+                        <div>{item.description}</div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </>
     )
 }

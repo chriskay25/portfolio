22d2b5d7dd0093538acf1529a7a95022f470d5b7
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-17 15:40:41 -0400

    Add motion to intro elements

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,23 +1,40 @@
 import Avatar from './Avatar'
 import Column from './Column'
 import SocialMedia from './SocialMedia'
+import { motion } from 'framer-motion'
+
+const item = {
+    initial: {
+        opacity: 0,
+        y: '40px',
+    },
+    animated: i => ({
+        opacity: 1,
+        y: 0,
+        transition: {
+            delay: i,
+            duration: .6,
+            ease: 'easeOut'
+        }
+    }),
+}
 
 const Intro = () => {
     return (
         <div className='intro'>
             <Column>
-                <h1 className='name'>Chris Kay</h1>
-                <div style={{height: '5px', width: '20%', background: 'var(--red)', marginBottom: '5px'}} />
+                <motion.h1 className='name' initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: .5, ease: 'easeIn'}}>Chris Kay</motion.h1>
+                <motion.div className='underline' initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: .7, ease: 'easeInOut'}} />
                 <SocialMedia />
                 <Avatar />
             </Column>
             <Column>
                 <div className='intro-description'>
-                    <h2>Full Stack Developer</h2>
-                    <p>
+                    <motion.h2 initial='initial' animate='animated' variants={item} custom='1.1'>Full Stack Developer</motion.h2>
+                    <motion.p initial='initial' animate='animated' variants={item} custom='1.3'>
                         Hello! My name is Chris Kay and I'm a full stack software engineer with a passion for technology.
                         I'm interested in finding a position as a junior developer at a company where I can learn from more experienced engineers and improve and expand my skillset. 
-                    </p>
+                    </motion.p>
                 </div>
             </Column>
         </div>

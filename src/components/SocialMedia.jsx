03dfb9629c2a7ba8988702cfdd41a17c7b06912f
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-17 15:41:18 -0400

    Make component fade in

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,5 +1,6 @@
 import liLogo from '../assets/linkedin-32.png'
 import ghLogo from '../assets/gh-light-sm.png'
+import { motion } from 'framer-motion'
 
 const SocialMedia = () => {
 
@@ -12,10 +13,10 @@ const SocialMedia = () => {
     }
 
     return (
-        <div className='social-media'>
+        <motion.div className='social-media' initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: .5, ease: 'easeIn'}}>
             <img src={liLogo} alt='linked-in-logo' onClick={() => handleLiClick} />
             <img src={ghLogo} alt='github-logo' onClick={() => handleGhClick()} />
-        </div>
+        </motion.div>
     )
 }
 

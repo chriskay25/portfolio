913dff49d84d9e2706794cc9a3f07721a08649a2
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-17 15:43:34 -0400

    Use motion to animate box shadow on scroll

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,17 +1,21 @@
 import Column from './Column'
 import Stack from './Stack'
 import Proficiencies from './Proficiencies'
+import { motion, useViewportScroll, useTransform } from 'framer-motion'
 
 const Skills = () => {
+    const { scrollYProgress } = useViewportScroll()
+    const glow = useTransform(scrollYProgress, [0, .2], ['0 0 0px 0px var(--red)', '0 0 14px 10px var(--red)'])
+
     return (
-        <div className='skills'>
+        <motion.div className='skills' style={{boxShadow: glow}}>
             <Column>
                 <Stack />
             </Column>
             <Column>
                 <Proficiencies />
             </Column>
-        </div>
+        </motion.div>
     )
 }
 

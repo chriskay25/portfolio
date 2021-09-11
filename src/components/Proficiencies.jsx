3880e7d11403f721db7d6dec3e033e00d61e6b67
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-09-10 23:53:26 -0400

    Add motion to elements so they fade in

--- src/components/Proficiencies.jsx
+++ src/components/Proficiencies.jsx
@@ -1,19 +1,43 @@
+import { motion } from 'framer-motion'
+
+const fadeIn = {
+    initial: {
+        opacity: 0,
+    },
+    animated: {
+        opacity: 1,
+        transition: {
+            when: 'beforeChildren',
+            staggerChildren: .2,
+            delay: 1.6,
+            duration: .6,
+        }
+    },
+}
+
+const item = {
+    initial: {opacity: 0, y: '90px',},
+    animated: {opacity: 1, y: 0, transition: {type: 'spring', stiffness: 30}}
+}
+
 const Proficiencies = () => {
     return (
         <div className='proficiencies'>
-            <h2>RELEVANT SKILLS & KNOWLEDGE</h2>
-            <ul>
-                <li>Object Oriented Programming</li>
-                <li>SQL Databases (Sqlite & Postgress)</li>
-                <li>Authentication (Rails, JWT's, OAuth)</li>
-                <li>Single Page Applications</li>
-                <li>MVC Frameworks</li>
-                <li>RESTful Applications</li>
-                <li>APIs</li>
-                <li>JSON</li>
-                <li>Git</li>
-                <li>Thunk</li>
-            </ul>
+            <motion.h2 initial={{opacity: 0, y: '40px'}} animate={{opacity: 1, y: 0}} transition={{delay: 2, duration: .6}}>
+                RELEVANT SKILLS & KNOWLEDGE
+            </motion.h2>
+            <motion.ul initial='initial' animate='animated' variants={fadeIn}>
+                <motion.li variants={item}>Object Oriented Programming</motion.li>
+                <motion.li variants={item}>SQL Databases (Sqlite & Postgress)</motion.li>
+                <motion.li variants={item}>Authentication (Rails, JWT's, OAuth)</motion.li>
+                <motion.li variants={item}>Single Page Applications</motion.li>
+                <motion.li variants={item}>MVC Frameworks</motion.li>
+                <motion.li variants={item}>RESTful Applications</motion.li>
+                <motion.li variants={item}>APIs</motion.li>
+                <motion.li variants={item}>JSON</motion.li>
+                <motion.li variants={item}>Git</motion.li>
+                <motion.li variants={item}>Thunk</motion.li>
+            </motion.ul>
         </div>
     )
 }

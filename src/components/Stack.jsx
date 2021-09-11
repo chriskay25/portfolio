b7ce3241a9e3da68625bbe3b2515bee461579cc6
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-09-10 23:52:32 -0400

    Add motion to stack elements so they fade in

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -3,6 +3,22 @@ import data from '../assets/data.js'
 import Card from './Card'
 import { motion, AnimateSharedLayout } from 'framer-motion'
 
+const fadeIn = {
+    initial: {
+        opacity: 0,
+        y: '40px',
+    },
+    animated: i => ({
+        opacity: 1,
+        y: 0,
+        transition: {
+            delay: i,
+            duration: .6,
+            ease: 'easeOut'
+        }
+    }),
+}
+
 const Stack = () => {
     const [selected, setSelected] = useState(false)
 
@@ -16,9 +32,11 @@ const Stack = () => {
 
     return (
         <div className='stack'>
-            <h2>MY LANGUAGES & FRAMEWORKS</h2>
+            <motion.h2 initial='initial' animate='animated' variants={fadeIn} custom='1.3' >
+                MY LANGUAGES & FRAMEWORKS
+            </motion.h2>
             <AnimateSharedLayout type='crossfade'>
-                <motion.ul layout>
+                <motion.ul layout initial='initial' animate='animated' variants={fadeIn} custom='1.7'>
                     {data.map((item) => (
                         <motion.li key={item.id} onClick={() => handleSelected(item.id)} animate={selected && selected !== item.id ? {opacity: 0} : {opacity: 1}}>
                             <Card key={item.id} item={item} selected={selected === item.id} />

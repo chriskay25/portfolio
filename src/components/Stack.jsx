92f91be461b4ffb795e8aa10d92ed9bb5f8c8d33
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-09-14 19:04:11 -0400

    Remove variant

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,23 +1,7 @@
 import React, { useState } from 'react'
-import data from '../assets/data.js'
+import { stackData } from '../assets/data.js'
 import Card from './Card'
 import { motion, AnimateSharedLayout } from 'framer-motion'
-
-const fadeIn = {
-    initial: {
-        opacity: 0,
-        y: '40px',
-    },
-    animated: i => ({
-        opacity: 1,
-        y: 0,
-        transition: {
-            delay: i,
-            duration: .6,
-            ease: 'easeOut'
-        }
-    }),
-}
 
 const Stack = () => {
     const [selected, setSelected] = useState(false)
@@ -32,12 +16,12 @@ const Stack = () => {
 
     return (
         <div className='stack'>
-            <motion.h2 initial='initial' animate='animated' variants={fadeIn} custom='1.3' >
+            <motion.h2 initial={{opacity: 0, y: '40px'}} animate={{opacity: 1, y: 0}} transition={{delay: 1.3, duration: .6}}>
                 MY LANGUAGES & FRAMEWORKS
             </motion.h2>
             <AnimateSharedLayout type='crossfade'>
-                <motion.ul layout initial='initial' animate='animated' variants={fadeIn} custom='1.7'>
-                    {data.map((item) => (
+                <motion.ul layout>
+                    {stackData.map((item) => (
                         <motion.li key={item.id} onClick={() => handleSelected(item.id)} animate={selected && selected !== item.id ? {opacity: 0} : {opacity: 1}}>
                             <Card key={item.id} item={item} selected={selected === item.id} />
                         </motion.li>

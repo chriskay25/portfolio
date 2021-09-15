4fb00f99d090c8e1a05e542b42a8b267884f62ad
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-09-14 23:59:09 -0400

    Use y offset to conditionally start animation

--- src/components/Proficiencies.jsx
+++ src/components/Proficiencies.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { motion, useViewportScroll, AnimatePresence, useTransform } from 'framer-motion'
+import React, { useState, useEffect, useRef } from 'react'
+import { motion, useViewportScroll, AnimatePresence } from 'framer-motion'
 import { proficiencyData } from '../assets/data'
 
 const fadeIn = {
@@ -24,29 +24,35 @@ const item = {
 }
 
 const Proficiencies = () => {
+    const ref = useRef()
+    const [visible, setVisible] = useState(false)
     const { scrollYProgress } = useViewportScroll()
 
     useEffect(() => {
+        if (ref.current.offsetTop < 900) setVisible(true)
         scrollYProgress.onChange(y => {
             console.log('ycurrent: ', y)
+            if (ref.current.offsetTop > 900) {
+                y > .15 && setVisible(true)
+                y < .15 && setVisible(false)
+            }
         })
-        console.log('scrollYProgress: ', scrollYProgress.current)
     })
 
     return (
-        <div className='proficiencies'>
+        <div className='proficiencies' ref={ref}>
             <motion.h2 initial={{opacity: 0, y: '40px'}} animate={{opacity: 1, y: 0}} transition={{delay: 2, duration: .6}}>
                 RELEVANT SKILLS & KNOWLEDGE
             </motion.h2>
-            <motion.ul initial='initial' animate='animated' variants={fadeIn}>
                 <AnimatePresence>
+            <motion.ul initial='initial' animate={visible ? 'animated' : 'initial'} variants={fadeIn}>
                     {proficiencyData.map((p, i) => (
                         <motion.li key={i} variants={item}>
                             {p.proficiency}
                         </motion.li>
                     ))}
-                </AnimatePresence>
             </motion.ul>
+                </AnimatePresence>
         </div>
     )
 }

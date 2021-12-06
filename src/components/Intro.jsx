6d5e8b0c5c16f3a2ec76c37e04653eee047c15ca
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-12-05 23:55:07 -0500

    Create svg Name component and add to Intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,5 @@
 import Avatar from './Avatar'
+import Name from './Name'
 import Column from './Column'
 import SocialMedia from './SocialMedia'
 import { motion } from 'framer-motion'
@@ -23,15 +24,15 @@ const Intro = () => {
     return (
         <div className='intro'>
             <Column>
-                <motion.h1 className='name' initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: .5, ease: 'easeIn'}}>Chris Kay</motion.h1>
-                <motion.div className='underline' initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: .7, ease: 'easeInOut'}} />
+                <Name />
+                <motion.div className='underline' initial={{opacity: 0, width: '0px'}} animate={{opacity: 1, width: '120px'}} transition={{delay: 2.7, duration: 1, type: 'spring', ease: 'easeInOut'}} />
                 <SocialMedia />
                 <Avatar />
             </Column>
             <Column>
                 <div className='intro-description'>
-                    <motion.h2 initial='initial' animate='animated' variants={item} custom='1.1'>FULL STACK DEVELOPER</motion.h2>
-                    <motion.p initial='initial' animate='animated' variants={item} custom='1.3'>
+                    <motion.h2 initial='initial' animate='animated' variants={item} custom='3.3'>FULL STACK DEVELOPER</motion.h2>
+                    <motion.p initial='initial' animate='animated' variants={item} custom='3.6'>
                         Hello! My name is Chris Kay and I'm a full stack software engineer with a passion for technology.
                         I'm interested in finding a position as a junior developer at a company where I can learn from more experienced engineers and improve and expand my skillset. 
                     </motion.p>

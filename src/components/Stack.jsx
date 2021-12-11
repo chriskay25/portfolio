d44531ce42e48f0df2167e5465824f0614e0443f
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-12-11 14:04:57 -0500

    Change motion transitions

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -16,11 +16,11 @@ const Stack = () => {
 
     return (
         <div className='stack'>
-            <motion.h2 initial={{opacity: 0, y: '40px'}} animate={{opacity: 1, y: 0}} transition={{delay: 1.3, duration: .6}}>
+            <motion.h2 initial={{opacity: 0, y: '40px'}} animate={{opacity: 1, y: 0}} transition={{delay: 4.2, duration: .6}}>
                 MY LANGUAGES & FRAMEWORKS
             </motion.h2>
             <AnimateSharedLayout type='crossfade'>
-                <motion.ul layout>
+                <motion.ul layout initial={{opacity: 0, y: 50}} animate={{opacity: 1, y: 0}} transition={{delay: 4.5, duration: .6, ease: 'easeInOut'}}>
                     {stackData.map((item) => (
                         <motion.li key={item.id} onClick={() => handleSelected(item.id)} animate={selected && selected !== item.id ? {opacity: 0} : {opacity: 1}}>
                             <Card key={item.id} item={item} selected={selected === item.id} />

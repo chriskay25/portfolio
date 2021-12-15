e825168658671204cd578d24f34d77599752d074
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-12-14 23:59:23 -0500

    Add Header component to take the place of Name

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Skills from './components/Skills'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
 import Footer from './components/Footer'
-import Name from './components/Name'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <Name />
+      <Header />
       <Intro />
       <Skills />
       <Experience />
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Name from './Name'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <Name />
+    </div>
+  )
+}
+
+export default Header

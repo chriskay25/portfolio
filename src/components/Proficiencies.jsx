69a6788aa05a0a032debbc1294b10cfabe0075ee
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-01-04 22:00:23 -0500

    Implement visibility based on scroll position and screen size

--- src/components/Proficiencies.jsx
+++ src/components/Proficiencies.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react'
 import { motion, useViewportScroll, AnimatePresence } from 'framer-motion'
 import { proficiencyData } from '../assets/data'
 
-const fadeIn = {
+const fadeIn = (wide) => ({
     initial: {
         opacity: 0,
     },
@@ -11,43 +11,47 @@ const fadeIn = {
         transition: {
             when: 'beforeChildren',
             staggerChildren: .2,
-            delay: 1.6,
-            duration: .6,
+            delay: wide ? '4s' : '.5s',
         }
     },
-}
+})
 
-const item = {
+const item =  {
     initial: {opacity: 0, y: '90px',},
     animated: {opacity: 1, y: 0, transition: {type: 'spring', stiffness: 30}},
-    exit: {opacity: 0, y: '90px'}
 }
 
 const Proficiencies = () => {
     const ref = useRef()
     const [visible, setVisible] = useState(false)
+    const [wide, setWide] = useState(false)
     const { scrollYProgress } = useViewportScroll()
 
     useEffect(() => {
-        if (ref.current.offsetTop < 900) setVisible(true)
+        if (ref.current.offsetTop < 800) {
+            setWide(true)
+            setVisible(true)
+        }
         scrollYProgress.onChange(y => {
-            console.log('ycurrent: ', y)
+            console.log(y)
+
             if (ref.current.offsetTop > 900) {
                 y > .15 && setVisible(true)
                 y < .15 && setVisible(false)
             }
         })
-    })
+    }, [scrollYProgress])
 
     return (
         <div className='proficiencies' ref={ref}>
-            <motion.h2 initial={{opacity: 0, y: '40px'}} animate={{opacity: 1, y: 0}} transition={{delay: 2, duration: .6}}>
+            <motion.h2 initial={{opacity: 0, y: '40px'}} animate={{opacity: 1, y: 0}} transition={{delay: 4.7, duration: .6}}>
                 RELEVANT SKILLS & KNOWLEDGE
             </motion.h2>
+
                 <AnimatePresence>
-            <motion.ul initial='initial' animate={visible ? 'animated' : 'initial'} variants={fadeIn}>
+            <motion.ul initial='initial' animate={visible ? 'animated' : 'initial'} variants={fadeIn} custom={wide}>
                     {proficiencyData.map((p, i) => (
-                        <motion.li key={i} variants={item}>
+                        <motion.li key={i} variants={item} custom={wide}>
                             {p.proficiency}
                         </motion.li>
                     ))}

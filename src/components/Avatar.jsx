2e1214a39c2e4aabcccbd8fa321bc1cbba291216
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-01-04 22:01:42 -0500

    Lengthen animation transition

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion'
 
 const Avatar = () => {
     return (
-        <motion.div className='avatar' initial={{opacity: 0, scale: .8}} animate={{opacity: 1, scale: 1}} transition={{delay: 0.3, duration: 1}}>
+        <motion.div className='avatar' initial={{opacity: 0, scale: .7}} animate={{opacity: 1, scale: 1}} transition={{delay: 1.2, duration: 1}}>
             <img src={dev} alt='dev-png' />
         </motion.div>
     )
